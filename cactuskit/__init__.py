04f2b3cfe3601cd36cea9190a117395d4ea299da
qerdinger <91551241+QuentinCraft@users.noreply.github.com>
2026-05-17 16:20:05 +0200

    feat: implements public package exports in __init__.py

---
+++ cactuskit/__init__.py
@@ -0,0 +1,21 @@
+"""CactusKit - Public package exports"""
+
+from .cactuskit import (
+	ApiMethod,
+	ApiProtocol,
+	CactusEncoder,
+	CactusResponse,
+	HttpStatus,
+	cactuize,
+	make_res,
+)
+
+__all__ = [
+	"ApiMethod",
+	"ApiProtocol",
+	"CactusEncoder",
+	"CactusResponse",
+	"HttpStatus",
+	"cactuize",
+	"make_res",
+]
